5bc722a23ad4584f131195822bf4c4c87f95fdba
Jeremy Kiley <spiderkiley4@gmail.com>
2024-10-16 21:20:08 -0400

    added placeholder video and elden ring png

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
+import VideoPlayer from './components/VideoPlayer';
+import video from "./assets/test.webm";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const lightP = ""
 
   return (
     <div className={'h-screen transition-colors duration-1000' + (darkMode ? " bg-[#242424]" : "")}>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setDarkMode((mode) => !mode)}>
+      <h1 className={'font-bold text-2xl text-center transition-colors duration-1000' + (darkMode ? " text-white" : "")}>Thomas Elden Ring Interview</h1>
+      <div className="">
+        <button onClick={() => setDarkMode((mode) => !mode)} className='absolute top-5 right-5 bg-blue-500 text-white text-xl py-2 px-4 rounded transition-all duration-300 transform hover:scale-110'>
           {darkMode ? "☀️" : "🌙"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="">
-        Click on the Vite and React logos to learn more
-      </p>
+        </div>
+        <VideoPlayer src={video} />
     </div>
   )
 }
---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function VideoPlayer({ src }) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <video
+        className="w-full max-w-3xl"
+        controls
+        src={src}
+        type="video/webm"
+        // Add additional attributes as needed
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
+
+export default VideoPlayer;
